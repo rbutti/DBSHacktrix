c14b7814106c01dabaecada82ed9babe71623e94
Arnab Sarkar <arnab.s1993@gmail.com>
2019-03-30 13:52:34 +0530

    jzxcb,

--- src/main/java/com/example/easynotes/controller/NoteController.java
+++ src/main/java/com/example/easynotes/controller/NoteController.java
@@ -15,29 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.easynotes.model.DefaulterList;
 import com.example.easynotes.model.LoanDetails;
+import com.example.easynotes.model.LoanRequest;
 import com.example.easynotes.model.LoginRequest;
 import com.example.easynotes.model.Note;
 import com.example.easynotes.model.PersonDetails;
 
 /**
- * Created by rajeevkumarsingh on 27/06/17.
+ * Created by ArnabSarkar on 30/03/19.
  */
 @RestController
 @RequestMapping("/api")
 public class NoteController {
-
-   
-
-    @GetMapping("/notes")
-    public List<Note> getAllNotes() {
-        return new ArrayList<Note>();
-    }
-    
-    
-//    @GetMapping("/loans")
-//    public List<Loan> getLoanStatus() {
-//        return new ArrayList<Loan>();
-//    }
     
     @PostMapping("/login")
     public ResponseEntity getLoginDetails(@RequestBody LoginRequest login) {
@@ -46,7 +34,11 @@ public class NoteController {
         return re;
     }
     
-
+    @PostMapping("/createLoanRequest")
+    public ResponseEntity createLoanRequest(@RequestBody LoanRequest loan) {
+    	ResponseEntity<LoanRequest> re = new ResponseEntity<LoanRequest>(HttpStatus.ACCEPTED);
+        return re;
+    }
     
     @PostMapping("/getDefaulterList")
     public ResponseEntity createNote(@RequestBody DefaulterList defaulter) {
@@ -71,34 +63,4 @@ public class NoteController {
     	LoanDetails  pr = new LoanDetails();
     	return ResponseEntity.ok(pr);
     }
-
-    /*@GetMapping("/notes/{id}")
-    public Note getNoteById(@PathVariable(value = "id") Long noteId) {
-        return noteRepository.findById(noteId)
-                .orElseThrow(() -> new ResourceNotFoundException("Note", "id", noteId));
-    }*/
-//
-//    @PutMapping("/notes/{id}")
-//    public Note updateNote(@PathVariable(value = "id") Long noteId,
-//                                           @Valid @RequestBody Note noteDetails) {
-//
-//        Note note = noteRepository.findById(noteId)
-//                .orElseThrow(() -> new ResourceNotFoundException("Note", "id", noteId));
-//
-//        note.setTitle(noteDetails.getTitle());
-//        note.setContent(noteDetails.getContent());
-//
-//        Note updatedNote = noteRepository.save(note);
-//        return updatedNote;
-//    }
-//
-//    @DeleteMapping("/notes/{id}")
-//    public ResponseEntity<?> deleteNote(@PathVariable(value = "id") Long noteId) {
-//        Note note = noteRepository.findById(noteId)
-//                .orElseThrow(() -> new ResourceNotFoundException("Note", "id", noteId));
-//
-//        noteRepository.delete(note);
-//
-//        return ResponseEntity.ok().build();
-//    }
 }
